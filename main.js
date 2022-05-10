d6dd94c420aac2be4c556d990899b8189e8daaf1
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-05-10 10:20:10 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const HeaderLinks = {
+	"Home":"index.html",
+};
 
+const HeaderNav = document.getElementById("header-nav");
+
+for(let Name in HeaderLinks){
+	let A = document.createElement("a");
+	A.href=HeaderLinks[Name];
+	A.innerHTML=Name;
+	HeaderNav.appendChild(A);
+}
