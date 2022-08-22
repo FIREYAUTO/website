acfcf27db901d046608585ab2f0502817988b1dc
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-08-22 08:26:55 -0700

    Update site.js

--- site.js
+++ site.js
@@ -9,6 +9,11 @@ window.onload = function(){
 	const siteTitle=document.head.querySelector("title"),
 	      pageName=getMetadata("page-name");
 	if(siteTitle&&pageName)siteTitle.innerHTML=`${pageName} - ${siteTitle.innerHTML}`;
+	let h=document.head;
+	let i=document.createElement("link");
+	i.rel="icon";
+	i.href="fs.png";
+	h.appendChild(i);
 }
 
 /*
